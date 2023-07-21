7aa3dbf8acefee4ac5ea9110d545255d26281425
Ivan Osuna <ivan.osuna.ayuste@gmail.com>
2023-07-21 11:43:04 +0200

    Commit

--- proyectos/prueba2/src/app/components/usuario-formulario/usuario.formulario.component.ts
+++ proyectos/prueba2/src/app/components/usuario-formulario/usuario.formulario.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormArray, FormBuilder, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+import { Observable, of } from 'rxjs';
 
 @Component({
     selector: 'usuario-formulario',                 // Nombre de la etiqueta HTML que estamos generando
@@ -9,18 +10,81 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 export class UsuarioFormularioComponent implements OnInit {
 
     formulario!: FormGroup
+    direcciones!: FormArray
 
     constructor(private formBuilder: FormBuilder) { }
 
     ngOnInit(): void {
+
+        this.direcciones = this.formBuilder.array([])
         this.formulario = this.formBuilder.group({
             nombre:    [null, [Validators.required, Validators.pattern("^[A-ZÁÉÍÓÚÑ][a-záéíóúñ]+(( [a-z]{1,4})* [A-ZÁÉÍÓÚÑ][a-záéíóúñ]+)*$")]],
             apellidos: [null, [Validators.required, Validators.minLength(2)]],
             edad:      [null, [Validators.min(0), Validators.max(130)]],
             email:     [null, [Validators.required, Validators.email]],
             conduce:   'no',
-            tienesVehiculo:   true
+            tienesVehiculo:   true,
+            genero:     'No lo digo',
+            direcciones: this.direcciones
         })
+        this.formulario.addAsyncValidators
+    }
+    nuevaDireccion(): void {
+
+        let direccion = this.formBuilder.group({
+            via:   null,
+            numero:   null,
+            cp:   [null, [], [UsuarioFormularioComponent.validarCP] ],
+            poblacion:   null,
+        })
+        this.direcciones.push(direccion);
+        // En cada control, damos 3 cosas:
+        // - Valor inicial
+        // - Validaciones
+        // - Validaciones asíncronas!
+    }
+
+    actualizacionFormulario() {
+        console.log(this.formulario)
+        //let valores = this.formulario.value;
+        //valores.direcciones
+        //for (let direccion of valores.direcciones)...
+    }
+
+    //static cpValidationTimeout: any= undefined;
+
+    static validarCP (campoCodigoPostal: AbstractControl):Observable<ValidationErrors | null>{
+        if(!campoCodigoPostal.value) return of(null);
+        let respuestaDelBack: Observable<boolean>;
+            respuestaDelBack=UsuarioFormularioComponent.validarCPMedianteLlamadaAServicioRest(campoCodigoPostal.value)
+
+        return new Observable( (subscriptor) => {
+            respuestaDelBack.subscribe( valido => {
+                if(valido) subscriptor.next(null)
+                else subscriptor.next({"cp_invalido": true})
+                subscriptor.complete()
+            })
+        })
+    }
+/*
+        console.log("Validando: "+campoCodigoPostal.value)
+        if(UsuarioFormularioComponent.cpValidationTimeout){
+            clearTimeout(UsuarioFormularioComponent.cpValidationTimeout)
+        }
+        setTimeout( () => UsuarioFormularioComponent.validarCPMedianteLlamadaAServicioRest(campoCodigoPostal.value), 2000);
+*/
+
+    static validarCPMedianteLlamadaAServicioRest(cp:string): Observable<boolean>{ // Esta función debe ser asíncrona!
+        // El servicio rest me devuelve si el cp es valido(true) o no (false)
+        // Qué debe devolver esta función?
+        return new Observable( (suscriptor) => {
+            setTimeout( ()=>suscriptor.next(cp.startsWith("2")), 2000 )
+        })
+        
+    }
+
+}
+
 
         /*
         this.formulario = new FormGroup({
@@ -41,10 +105,3 @@ export class UsuarioFormularioComponent implements OnInit {
                 Validators.email
             ])
         })*/
-    }
-
-    actualizacionFormulario() {
-        console.log(this.formulario)
-    }
-
-}
